dcc7c671b8fcea02dc930f81efa52178ca6b6de9
Brett Weaver <brett@weav.net>
2013-08-16 13:09:40 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# s3go
+
+CLI to interact with S3 written in Go.
+
+# Setup
+
+Set environment variables:
+
+    AWS_SECRET_ACCESS_KEY=yyy
+    AWS_ACCESS_KEY_ID=xxx
+
+# Usage
+
+## ls
+
+s3go ls bucket_name
---
+++ s3go.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+    "fmt"
+    "github.com/codegangsta/cli"
+    "launchpad.net/goamz/s3"
+    "launchpad.net/goamz/aws"
+    "os"
+)
+
+func main() {
+    app := cli.NewApp()
+    app.Name = "s3go"
+    app.Usage = "CLI for S3"
+
+    app.Flags = []cli.Flag {
+      cli.StringFlag{"lang", "english", "language for the greeting"},
+    }
+
+    app.Commands = []cli.Command{
+      {
+        Name:      "ls",
+        Usage:     "List contents of bucket.",
+        Action: func(c *cli.Context) {
+          ListBucketContents(c.Args()[0])
+        },
+      },
+    }
+
+    app.Run(os.Args)
+}
+
+func ListBucketContents(bucketName string) {
+    auth := ConnectS3()
+    s := s3.New(auth, aws.USEast)
+    bucket := s.Bucket(bucketName)
+    data, err := bucket.List("", "", "", 0)
+    if err != nil {
+        panic(err.Error())
+    }
+
+    for key := range data.Contents {
+        fmt.Printf("s3://%s/%s\n", bucketName, data.Contents[key].Key)
+    }
+}
+
+func ConnectS3() (auth aws.Auth)  {
+    // The AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY environment variables are used.
+    auth, err := aws.EnvAuth()
+    if err != nil {
+      panic(err.Error())
+    }
+    return auth
+}
