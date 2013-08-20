e613961f2cafc874a487ea86231e268f1c2b73c3
Brett Weaver <brett@weav.net>
2013-08-20 07:44:12 -0700

    first crusty ghetto test running

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+all: deps
+	@mkdir -p bin/
+	go build -o bin/s3go .
 deps:
 	@echo "Installing Deps"
 	go get -d -v ./...
--- actions/list_bucket_contents_test.go
+++ actions/list_bucket_contents_test.go
@@ -1,10 +1,14 @@
 package s3go
 
+import "os"
 import "testing"
 
 func TestConnectS3(t *testing.T) {
     auth := ConnectS3()
-    if 1 != 2 {
-        t.Error("Expected 1 got 2")
+    if auth.AccessKey != os.Getenv("AWS_ACCESS_KEY_ID") {
+        t.Error("Could not read AWS_ACCESS_KEY_ID.")
+    }
+    if auth.SecretKey != os.Getenv("AWS_SECRET_ACCESS_KEY") {
+        t.Error("Could not read AWS_SECRET_ACCESS_KEY.")
     }
 }
