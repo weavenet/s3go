5f6793fc3aabc196600f71ae5f59f1e42c237b38
Brett Weaver <brett@weav.net>
2013-08-22 07:23:34 -0700

    updated test

--- s3go/s3_test.go
+++ s3go/s3_test.go
@@ -2,14 +2,28 @@ package s3go
 
 import "testing"
 
+type testpair struct {
+    url string
+    bucket string
+    key string
+}
+
+var tests = []testpair {
+    { "s3://bucket/test.tar.gz", "bucket", "test.tar.gz" },
+    { "s3://bucket-123/dir/folder/key", "bucket-123", "dir/folder/key" },
+    { "bucket-123/dir/folder/key", "bucket-123", "dir/folder/key" },
+}
+
 func TestS3Url(t *testing.T) {
-    url := S3Url{Url: "s3://bucket1/dir/test123"}
+    for _, pair := range tests {
+        url := S3Url{Url: pair.url}
 
-    if url.Key() != "dir/test123" {
-      t.Error("Key not returned correctly.")
-    }
+        if url.Key() != pair.key {
+          t.Error("Key not returned correctly.")
+        }
 
-    if url.Bucket() != "bucket1" {
-      t.Error("Bucket not returned correctly.")
+        if url.Bucket() != pair.bucket {
+          t.Error("Bucket not returned correctly.")
+        }
     }
 }
