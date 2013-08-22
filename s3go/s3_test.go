f31e86259948d9e3576d2744e5a714cc5f7dffff
Brett Weaver <brett@weav.net>
2013-08-22 10:48:14 -0700

    adding test cases

--- s3go/s3_test.go
+++ s3go/s3_test.go
@@ -2,13 +2,13 @@ package s3go
 
 import "testing"
 
-type testpair struct {
+type testcase struct {
     url string
     bucket string
     key string
 }
 
-var tests = []testpair {
+var tests = []testcase {
     { "s3://bucket/test.tar.gz", "bucket", "test.tar.gz" },
     { "s3://bucket-123/dir/folder/key", "bucket-123", "dir/folder/key" },
     { "s3://bucket-123/files*", "bucket-123", "files*" },
@@ -17,14 +17,14 @@ var tests = []testpair {
 }
 
 func TestS3Url(t *testing.T) {
-    for _, pair := range tests {
-        url := S3Url{Url: pair.url}
+    for _, c := range tests {
+        url := S3Url{Url: c.url}
 
-        if url.Key() != pair.key {
+        if url.Key() != c.key {
           t.Error("Key not returned correctly.")
         }
 
-        if url.Bucket() != pair.bucket {
+        if url.Bucket() != c.bucket {
           t.Error("Bucket not returned correctly.")
         }
     }
