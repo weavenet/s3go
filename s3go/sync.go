bcc5edf6945b9d8cae5cf41c7f4239a3d700d5cc
Brett Weaver <brett@weav.net>
2013-08-27 20:42:46 -0700

    working on syncing

--- s3go/sync.go
+++ s3go/sync.go
@@ -1,6 +1,8 @@
 package s3go
 
 import (
+    //"crypto/sha1"
+    "io"
     "os"
     "path/filepath"
     "strings"
@@ -16,30 +18,57 @@ type SyncPair struct {
 func (s *SyncPair) Sync() bool {
     if s.validPair() {
         if validS3Url(s.Source) {
-           loadFiles("/etc/")
            s.syncS3ToDir()
+           return true
         } else {
            s.syncDirToS3()
+           return true
         }
     }
+    println("Path not valid.")
     return false
 }
 
-func loadFile(path string, f os.FileInfo, err error) error {
-  println("%s", path)
-  return nil
-} 
-
-func loadFiles(path string) {
-  filepath.Walk(path, loadFile)
-}
-
 func (s *SyncPair) syncDirToS3() bool {
+    loadFiles(s.Source)
     return true
 }
 
 func (s *SyncPair) syncS3ToDir() bool {
+    loadFiles(s.Target)
     return true
+}
+
+func loadFile(path string, f os.FileInfo, e error) error {
+  return nil
+}
+
+func loadFiles(path string) map[string]string {
+    files := map[string]string{}
+    filepath.Walk(path, func(file_path string, info os.FileInfo, err error) error {
+        if !info.IsDir() {
+            // Add path to files
+            fi, err := os.Open(path)
+            if err != nil {
+                panic(err)
+            }
+            buf := make([]byte, 1024)
+            for {
+                // read a chunk
+                n, err := fi.Read(buf)
+                if err != nil && err != io.EOF { panic(err) }
+                if n == 0 { break }
+            }
+
+            //files[file_path]
+            // read the file into content here
+            println(file_path)
+            println(buf)
+            println(fi)
+        }
+        return nil
+    })
+    return files
 }
 
 func (s *SyncPair) validPair() bool {
